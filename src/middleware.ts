19d84fc987d74fd7cdc6b6bb0bb8abf6af6905fd
andrew sòlanto <atsolanto@gmail.com>
2025-02-15 19:40:12 -0500

    use edge-friendly route matching in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,68 +4,75 @@ import "@atcute/bluesky/lexicons"
 import { sequence } from "astro:middleware"
 import { isValidHandle } from "@atproto/syntax"
 
-const removeEmptyQuery: MiddlewareHandler = async (
+const removeEmptyQuery = (async (
 	{ url: { href, pathname }, redirect },
 	next
 ) => {
 	if (href.endsWith("?")) return redirect(pathname)
 
 	return await next()
-}
+}) satisfies MiddlewareHandler
 
-const map: Record<string, MiddlewareHandler> = {
-	"/": removeEmptyQuery,
-	"/[profile]": sequence(
-		async (
-			{ params: { profile }, locals, rewrite },
-			next
-		) => {
-			if (!isValidHandle(profile as string))
-				return rewrite("/404")
+const profileHandler = sequence(
+	async (
+		{ params: { profile }, locals, rewrite },
+		next
+	) => {
+		if (!isValidHandle(profile as string))
+			return rewrite("/404")
 
-			try {
-				const {
-					data: { labels }
-				} = await publicClient.get(
-					"app.bsky.actor.getProfile",
-					{
-						params: {
-							actor: profile as string
-						}
+		try {
+			const {
+				data: { labels }
+			} = await publicClient.get(
+				"app.bsky.actor.getProfile",
+				{
+					params: {
+						actor: profile as string
 					}
-				)
+				}
+			)
 
-				locals.isProfilePublic = Boolean(
-					labels &&
-						!labels.some(
-							({ val }) =>
-								val == "!no-unauthenticated"
-						)
-				)
-			} catch (error) {
-				if ((error as any).status == 400)
-					return rewrite(
-						"/error/profile-not-found"
+			locals.isProfilePublic = Boolean(
+				labels &&
+					!labels.some(
+						({ val }) =>
+							val == "!no-unauthenticated"
 					)
-			}
+			)
+		} catch (error) {
+			if ((error as any).status == 400)
+				return rewrite("/error/profile-not-found")
+		}
 
-			return await next()
-		},
-		removeEmptyQuery
-	)
-}
+		return await next()
+	},
+	removeEmptyQuery
+) as MiddlewareHandler
 
-export const onRequest: MiddlewareHandler = async (
-	context,
-	next
-) => {
-	const handler = map[context.routePattern]
+const nonProfilePathnameMatchers = [
+	/^\/$/,
+	/^\/favicon\.svg$/,
+	/^\/jwks\.json$/,
+	/^\/client-metadata\.json$/,
+	/^\/profile$/,
+	/^\/styles\/.*$/,
+	/^\/images\/.*$/,
+	/^\/login$/,
+	/^\/login\/redirect$/,
+	/^\/logout$/
+]
 
+export const onRequest = (async (context, next) => {
 	console.log(context)
 
-	return await (handler ?
-		(handler(context, next) as
-			| Response
-			| Promise<Response>)
-	:	next())
-}
+	if (context.url.pathname == "/")
+		return await removeEmptyQuery(context, next)
+	else if (
+		!nonProfilePathnameMatchers.some(matcher =>
+			matcher.test(context.url.pathname)
+		)
+	)
+		return await profileHandler(context, next)
+	else return await next()
+}) as MiddlewareHandler
